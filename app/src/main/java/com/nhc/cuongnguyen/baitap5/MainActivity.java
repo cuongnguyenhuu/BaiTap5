59ee810d59fed9a831158a94207748c6847421c0
PHI-LONG-PLAZA\PC <cuongnguyenhuu97@gmail.com>
2018-02-02 17:03:48 +0700

    Initial commit

---
+++ app/src/main/java/com/nhc/cuongnguyen/baitap5/MainActivity.java
@@ -0,0 +1,53 @@
+package com.nhc.cuongnguyen.baitap5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listTinh;
+    ArrayList<String> arrayTinh;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listTinh = (ListView) findViewById(R.id.listTinh);
+        arrayTinh = new ArrayList<>();
+        arrayTinh.add("Ha Noi");
+        arrayTinh.add("Ha Nam");
+        arrayTinh.add("Ha Tinh");
+        arrayTinh.add("Nghe An");
+        arrayTinh.add("Quang Binh");
+        arrayTinh.add("Quang Tri");
+        arrayTinh.add("Hue");
+        arrayTinh.add("Da Nang");
+        arrayTinh.add("Quang Nam");
+        arrayTinh.add("Quang Ngai");
+        arrayTinh.add("Binh Dinh");
+        arrayTinh.add("Phu Yen");
+        arrayTinh.add("Khanh Hoa");
+        arrayTinh.add("Binh Thuan");
+        arrayTinh.add("Ba Ria-Vung Tau");
+        arrayTinh.add("Ho Chi Minh");
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,arrayTinh);
+        listTinh.setAdapter(adapter);
+
+        listTinh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this,ProvinceActivity.class);
+                intent.putExtra("tinh",arrayTinh.get(i));
+                startActivity(intent);
+            }
+        });
+    }
+}
